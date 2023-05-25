d9b6bebacb8ec11acb9d46a7ffd1572835c3617c
hazem_boumnijel <hazemboumnijel@gmail.com>
2023-05-25 09:14:46 +0100

    25/05/23

--- CSI-Dashboard/src/app/views/Component/HumanResource/projet/viewProjet/tasks/KanbanBoard.component.ts
+++ CSI-Dashboard/src/app/views/Component/HumanResource/projet/viewProjet/tasks/KanbanBoard.component.ts
@@ -1,6 +1,8 @@
 // kanban-board.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { ActivatedRoute } from '@angular/router';
+import { ProjetService } from '../../projet.service';
 
 
 @Component({
@@ -8,39 +10,65 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
   templateUrl: './kanbanBoard.component.html',
   styleUrls: ['./kanBoard.component.css']
 })
-export class KanbanBoardComponent {
+export class KanbanBoardComponent implements OnInit {
+  id :number
+  projet : any
+  constructor(
+    private router : ActivatedRoute,
+    private crudService: ProjetService,
+    
+    
+   
+  ) { }
+
+  ngOnInit() {
+    this.id = this.router.snapshot.params['id'];
+    this.getItem()
+  }
+  getItem(){
+    this.crudService.getItem(this.id).subscribe((data:any) =>{
+this.projet=data
+    })
+}
   tasks: any[] = [
     {
       
-      title: 'Task 1',
-      description: 'Complete task 1',
-      status: 'To Do',
+      title: 'Tache 1',
+      description: 'Crud',
+      status: 'A faire',
       assignee: 'John Doe'
     },
     {
       
-      title: 'Task 2',
-      description: 'Work on task 2',
-      status: 'In Progress',
+      title: 'Tache 2',
+      description: 'travailler sur tache 2',
+      status: 'En cours',
       assignee: 'Jane Smith'
     },
     {
      
-      title: 'Task 3',
-      description: 'Finish task 3',
-      status: 'Done',
+      title: 'Tache 3',
+      description: 'completer tache 3',
+      status: 'Terminé',
       assignee: 'John Doe'
     },
     {
      
-      title: 'Task 4',
-      description: 'Finish task 3',
-      status: 'Testing',
+      title: 'Tache 4',
+      description: 'Finir tache 4',
+      status: 'A faire',
       assignee: 'John Doe'
+    },
+    {
+      
+      title: 'Tache 5',
+      description: 'test',
+      status: 'Test',
+      assignee: 'Hazem'
     }
   ];
 
-  statuses: string[] = ['To Do', 'In Progress','Testing' ,'Done'];
+  statuses: string[] = ['A faire', 'En cours','Test' ,'Terminé'];
 
   getTasksByStatus(status: string): Task[] {
     return this.tasks.filter(task => task.status === status);
@@ -56,5 +84,16 @@ export class KanbanBoardComponent {
     this.tasks[taskIndex].status = status;
     moveItemInArray(this.tasks, taskIndex, event.currentIndex);
   }*/
-
+  getStatusClass(status: string): string {
+    switch (status) {
+      case 'A faire':
+        return 'status-todo';
+      case 'En cours':
+        return 'status-in-progress';
+      case 'Terminé':
+        return 'status-done';
+      default:
+        return '';
+    }
+  }
 }
